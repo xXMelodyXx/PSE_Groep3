085ebb094d958f91cf5f312ca7f65d75474c8fc9
Melody Sjauw Koen Fa <75821991+xXMelodyXx@users.noreply.github.com>
2026-05-23 12:07:47 +0200

    Add files via upload

---
+++ lijnsensor.h
@@ -0,0 +1,47 @@
+#include "WString.h"
+#include <Zumo32U4.h>
+
+class Lijnsensor {
+private:
+  Zumo32U4LineSensors lineSensors;
+
+  unsigned int sensorValues[5];
+
+  int blackValue[5];
+  int blackMin[5];
+  int blackMax[5];
+
+  int greenValue[5];
+  int greenMin[5];
+  int greenMax[5];
+
+  int brownValue[5];
+  int brownMin[5];
+  int brownMax[5];
+
+  int grayValue[5];
+  int grayMin[5];
+  int grayMax[5];
+
+  /* TO DO array voor kleuren 
+  int color_Value[5][4];
+  int color_Min[5][4];
+  int color_Max[5][4];
+  */
+
+  long totaal = 0;
+
+ void calibreer(bool debug, int waarde_min[5], int waarde_max[5]);
+
+public:
+  Lijnsensor();
+  void init();
+  void calibrateWhite(bool debug);
+  void calibrateBlack(bool debug);
+  void calibrateGreen(bool debug);
+  void calibrateBrown(bool debug);
+  void calibrateGray(bool debug);
+  void read(bool debug);
+  void read(bool debug, int sensorNr);
+  String bepaalRichting();
+};
